289e7aafbd622eb04e496e2e4e14b6a13d859d19
T Naresh <tnaresh564@gmail.com>
2025-03-29 16:12:49 +0530

    new changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1272613884"
+    serviceWorkerVersion: "65801938"
   }
 });
